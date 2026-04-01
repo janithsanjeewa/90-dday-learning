e3ee7e36eb19a651f4ff70c958f47b396e050fba
Janith Sanjeewa <janithsanjeewa95@gmail.com>
2026-04-01 22:05:54 +0200

    Fix folder structure

---
+++ week1-python/day2.py
@@ -0,0 +1,9 @@
+# Day 2 Python Practice
+
+print("Hello World")
+
+name = "Janith"
+print(name)
+
+for i in range(5):
+    print(i)
--- week1-sql/week1-python/day2.py
+++
@@ -1,9 +0,0 @@
-# Day 2 Python Practice
-
-print("Hello World")
-
-name = "Janith"
-print(name)
-
-for i in range(5):
-    print(i)
